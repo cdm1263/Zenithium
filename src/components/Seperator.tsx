ac8436916fdb1676284991a6692c20ba3dc70cd4
ChaDongmin <cdm1263@gmail.com>
2024-09-22 00:59:18 +0900

    feat: Seperator 제작

---
+++ src/components/Seperator.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+type Props = HTMLAttributes<HTMLDivElement> & {
+  className?: string;
+};
+
+const Seperator = ({ className, ...props }: Props) => {
+  return (
+    <div
+      className={cn("border-l border-muted-foreground/30", className)}
+      {...props}
+    />
+  );
+};
+
+export default Seperator;
