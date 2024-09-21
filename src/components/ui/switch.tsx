7a70d8d2bcf938941e6750135063f1b06b28083c
ChaDongmin <cdm1263@gmail.com>
2024-09-22 03:19:34 +0900

    design: 토글 트랜지션 타이밍 수정

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -29,10 +29,14 @@ const Switch = forwardRef<
     setTheme(currentTheme === "dark" ? "light" : "dark");
   };
 
+  const customTransition =
+    "[transition:background-color_.3s_cubic-bezier(0.25,0.46,0.45,0.94),transform_.2s_cubic-bezier(0.25,0.46,0.45,0.94),max-width_.1s_cubic-bezier(0.25,0.46,0.45,0.94)]";
+
   return (
     <SwitchPrimitives.Root
       className={cn(
-        "peer inline-flex h-6 w-12 shrink-0 cursor-pointer items-center rounded-full border-[1.5px] transition-colors border-foreground/10 shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-muted data-[state=unchecked]:bg-input duration-300",
+        "peer inline-flex h-6 w-12 shrink-0 cursor-pointer items-center rounded-full border-[1.5px] border-foreground/10 shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-muted data-[state=unchecked]:bg-input",
+        customTransition,
         className
       )}
       {...props}
@@ -42,12 +46,12 @@ const Switch = forwardRef<
     >
       <SwitchPrimitives.Thumb
         className={cn(
-          "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 relative duration-300"
+          "pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 relative duration-300"
         )}
       >
         <Moon
           className={cn(
-            "absolute h-2.5 w-2.5 m-auto inset-0 transition-all duration-0",
+            "absolute h-2.5 w-2.5 m-auto inset-0",
             `${
               mount
                 ? theme === "dark"
@@ -59,7 +63,7 @@ const Switch = forwardRef<
         />
         <Sun
           className={cn(
-            "absolute h-2.5 w-2.5 m-auto inset-0 transition-all duration-0",
+            "absolute h-2.5 w-2.5 m-auto inset-0",
             `${
               mount
                 ? theme === "light"
