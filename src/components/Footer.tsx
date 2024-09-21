3822fa23d0fb9a32bb338f6e50744b0219bf17c9
ChaDongmin <cdm1263@gmail.com>
2024-09-22 02:31:42 +0900

    feat: Footer 구성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import { Rss } from "lucide-react";
+import Inner from "./Inner";
+import Github from "/public/github.svg";
+import SVGtoComponent from "./SVGtoComponent";
+import { cn } from "@/lib/utils";
+
+const Footer = () => {
+  const iconStyle =
+    "h-7 w-7 text-foreground hover:text-muted-foreground transition-all cursor-pointer";
+
+  return (
+    <footer className="bg-muted h-60">
+      <Inner className="flex flex-col justify-center items-center">
+        <div className="flex gap-x-5 mb-10">
+          {/* // TODO: 추후 Link로 변경해야함 */}
+          <Rss className={iconStyle} />
+          <SVGtoComponent
+            SVG={Github}
+            className={cn(
+              iconStyle,
+              "fill-foreground hover:fill-muted-foreground"
+            )}
+          />
+        </div>
+        <p className="text-lg text-muted-foreground">
+          &copy; {new Date().getFullYear()}, All rights reserved.
+        </p>
+      </Inner>
+    </footer>
+  );
+};
+
+export default Footer;
