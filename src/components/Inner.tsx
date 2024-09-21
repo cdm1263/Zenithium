298b48c64962c79f19435c3ae8fbe798294ec77c
ChaDongmin <cdm1263@gmail.com>
2024-09-21 20:43:55 +0900

    feat: Inner 제작

---
+++ src/components/Inner.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+type Props = {
+  className?: string;
+  children: ReactNode;
+};
+
+const Inner = ({ className, children }: Props) => {
+  return (
+    <div
+      className={cn(
+        "h-full w-full max-w-screen-xl px-2 md:px-20 mx-auto",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Inner;
