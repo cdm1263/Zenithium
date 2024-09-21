902c83b7c9a156be5b8c75f7d7f94e1f163378d7
ChaDongmin <cdm1263@gmail.com>
2024-09-22 02:30:28 +0900

    feat: svg를 컴포넌트로 사용하기 위한 웹팩 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Note: svg를 컴포넌트로 사용
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
