b8c75b7cedeefcef2d876c16f64f7b28ce418a37
ChaDongMin <cdm1263@gmail.com>
2024-09-22 22:39:17 +0900

    feat: 블로그 페이지 임시 구성

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,24 @@
+import PostLists from "@/components/PostLists";
+import { getAllPosts } from "./utils";
+import Inner from "@/components/Inner";
+
+export const metadata = {
+  title: "Blog - 개발 블로그 Zenithium",
+  description: "To Zenith, 최고의 개발자를 향해",
+};
+
 const Page = () => {
-  return <div>Page</div>;
+  // TODO: 날짜순 정렬해야됨
+  const posts = getAllPosts();
+  return (
+    <section>
+      <Inner>
+        <div className="grid">
+          <PostLists posts={posts} />
+        </div>
+      </Inner>
+    </section>
+  );
 };
 
 export default Page;
