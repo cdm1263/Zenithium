57738359293a42ccde0096901ef4716bbb8319fc
ChaDongMin <cdm1263@gmail.com>
2024-09-22 22:38:34 +0900

    feat: 개별 포스트 페이지 기본 구성

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,50 @@
-const Blog = () => {
-  return <div>Blog</div>;
+import { notFound } from "next/navigation";
+import { getAllPosts } from "../utils";
+import Inner from "@/components/Inner";
+import CustomMDX from "@/components/CustomMDX";
+
+type Props = { params: { slug: string } };
+
+export const generateStaticParams = async () => {
+  const posts = getAllPosts();
+
+  if (!posts) return [];
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+};
+
+// TODO: 동적 메타데이터 관리 필요
+
+// export const generateMetadata = ({ params }) => {
+//   const posts = getAllPosts();
+//   if (!posts) return;
+
+// };
+
+const Blog = async ({ params }: Props) => {
+  const post = getAllPosts().find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <section>
+      <Inner className="flex">
+        {/* // TODO: JSON LD 관리 필요 */}
+        {/* <script></script> */}
+
+        <div className="hidden w-60 xl:block bg-yellow-200 shrink-0"></div>
+        <article className="flex-1 prose">
+          <h1>{post.frontMatter.title}</h1>
+          <CustomMDX source={post.content} />
+        </article>
+        <div className="hidden w-60 lg:block bg-yellow-200 shrink-0"></div>
+      </Inner>
+    </section>
+  );
 };
 
 export default Blog;
