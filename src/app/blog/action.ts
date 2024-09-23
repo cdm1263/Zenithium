784e284ba82633aab64a5d023b76c1486d1605aa
ChaDongmin <cdm1263@gmail.com>
2024-09-24 08:42:10 +0900

    feat: 포스트를 필터링할 서버액션 filterPosts 함수 구성

---
+++ src/app/blog/action.ts
@@ -0,0 +1,59 @@
+// 서버 액션 정의 부분
+"use server";
+
+import { getAllPosts } from "./utils";
+
+type Props = {
+  series: string;
+  tags: string | string[];
+  search: string;
+  sort: string;
+};
+
+export const filterPosts = async ({ series, tags, search, sort }: Props) => {
+  const originalPosts = getAllPosts();
+  let posts = originalPosts;
+
+  // Note: 모든 태그
+  const allTags = [
+    ...new Set(originalPosts.map((post) => post.frontMatter.tags).flat()),
+  ];
+
+  // Note: 태그 필터링
+  if (tags) {
+    posts = posts.filter((post) => {
+      if (typeof tags === "string") {
+        return post.frontMatter.tags.includes(tags);
+      } else {
+        return tags.some((tag) => post.frontMatter.tags.includes(tag));
+      }
+    });
+  }
+
+  // Note: 시리즈 필터링
+  if (series) {
+    posts = posts.filter((post) => post.frontMatter.series.includes(series));
+  }
+
+  // Note: 검색 필터링
+  if (search) {
+    const searchLower = search.toLowerCase();
+    posts = posts.filter(
+      (post) =>
+        post.frontMatter.title.toLowerCase().includes(searchLower) ||
+        post.frontMatter.description.toLowerCase().includes(searchLower)
+    );
+  }
+
+  // Note: 정렬
+  posts.sort((a, b) => {
+    if (sort === "oldest") {
+      return new Date(a.frontMatter.date) > new Date(b.frontMatter.date)
+        ? 1
+        : -1;
+    }
+    return new Date(a.frontMatter.date) > new Date(b.frontMatter.date) ? -1 : 1;
+  });
+
+  return posts;
+};
