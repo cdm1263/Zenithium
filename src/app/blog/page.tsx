43b33df2c02d5f91b834cb70ade095300c4392e8
ChaDongmin <cdm1263@gmail.com>
2024-09-24 08:43:35 +0900

    feat: blog 페이지에 Filter 적용 및 필터링 된 데이터 적용

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,30 @@
 import PostLists from "@/components/PostLists";
-import { getAllPosts } from "./utils";
+import { getAllTagsAndSeries } from "./utils";
 import Inner from "@/components/Inner";
+import Filter from "@/components/Filter";
+import { filterPosts } from "./action";
 
 export const metadata = {
   title: "Blog - 개발 블로그 Zenithium",
   description: "To Zenith, 최고의 개발자를 향해",
 };
 
-const Page = () => {
-  // TODO: 날짜순 정렬해야됨
-  const posts = getAllPosts();
+type Props = {
+  searchParams: { [key: string]: string };
+};
+
+export const revalidate = 0;
+
+const Page = async ({ searchParams }: Props) => {
+  const { series, tag: tags, search, sort } = searchParams;
+  const filteredPosts = await filterPosts({ series, tags, search, sort });
+  const { allTags, allSeries } = getAllTagsAndSeries();
+
   return (
     <section>
-      <Inner>
-        <div className="grid">
-          <PostLists posts={posts} />
-        </div>
+      <Inner className="flex flex-col lg:flex-row gap-5">
+        <Filter tags={allTags} series={allSeries} />
+        <PostLists posts={filteredPosts} />
       </Inner>
     </section>
   );
