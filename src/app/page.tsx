d98a619f72e75e9c6d62358456f4f1a678f4b021
ChaDongmin <cdm1263@gmail.com>
2024-09-24 23:36:42 +0900

    feat: 메인 페이지에 PostsList 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+import Inner from "@/components/Inner";
+import PostLists from "@/components/PostLists";
+import { getAllPosts } from "./blog/utils";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Home</div>;
+  const posts = getAllPosts()
+    .sort((a, b) => {
+      return new Date(b.frontMatter.date) > new Date(a.frontMatter.date)
+        ? 1
+        : -1;
+    })
+    .slice(0, 3);
+
+  return (
+    <>
+      <Inner>
+        <PostLists posts={posts} postsLength={false} />
+        <Link
+          href="/blog"
+          className={buttonVariants({
+            size: "lg",
+          })}
+        >
+          더 보기
+        </Link>
+      </Inner>
+    </>
+  );
 };
 
 export default Home;
