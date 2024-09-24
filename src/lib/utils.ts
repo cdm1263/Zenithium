6d7f439ec339c6cbad6790f9dfb4877e99425193
ChaDongmin <cdm1263@gmail.com>
2024-09-24 20:50:04 +0900

    feat: 날짜 포맷 유틸함수 formatDate 생성

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const currentYear = date.getUTCFullYear();
+  const currentMonth = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const currentDate = date.getUTCDate().toString().padStart(2, "0");
+
+  return `${currentYear}년 ${currentMonth}월 ${currentDate}일`;
+};
