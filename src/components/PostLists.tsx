30fbcb09455588e956928b168bf67992482ca2b7
ChaDongmin <cdm1263@gmail.com>
2024-09-24 23:35:54 +0900

    refactor: PostLists 컴포넌트 스타일 및 구조 개편

--- src/components/PostLists.tsx
+++ src/components/PostLists.tsx
@@ -3,68 +3,91 @@ import { formatDate } from "@/lib/utils";
 import { CalendarDays, Clock } from "lucide-react";
 import Link from "next/link";
 import readingTime from "reading-time";
+import Seperator from "./Seperator";
 
 type Props = {
   posts: Mdx[];
+  postsLength?: boolean;
 };
 
 const PostCard = ({
   frontMatter,
   timeToRead,
+  slug,
 }: {
   frontMatter: FrontMatter;
   timeToRead: number;
+  slug: string;
 }) => {
   const { series, title, description, date, tags } = frontMatter;
   return (
-    <div className="px-6 py-4 rounded-lg border-2 hover:bg-primary/5 hover:border-primary/60 transition-colors group">
-      <div className="flex w-full justify-between mb-1 ">
-        <p className="text-2xl font-bold">{title}</p>
-        <p className="text-sm border-2 border-muted-foreground/50 w-fit h-fit px-2 py-0.5 rounded-md shrink-0">
+    <div className="flex py-4 gap-5 w-full">
+      <Link
+        href={`/blog/${slug}`}
+        className="min-w-[200px] border-2 rounded-lg flex justify-center items-center"
+      >
+        이미지
+      </Link>
+      <div className="flex flex-col">
+        <Link
+          href={`/blog/?series=${series}`}
+          className="text-sm hover:underline text-muted-foreground font-semibold"
+        >
           {series}
-        </p>
-      </div>
-      <p className="text-lg text-muted-foreground font-semibold mb-2 ">
-        {description}
-      </p>
-      <div className="flex gap-2 mb-2">
-        {tags.map((tag) => (
-          <span key={tag} className="text-primary text-sm">
-            #{tag}
-          </span>
-        ))}
-      </div>
-      <div className="flex w-full justify-between text-sm text-muted-foreground">
-        <p className="flex gap-2 items-center">
-          <CalendarDays className="w-4 h-4" />
-          {formatDate(date)}
-        </p>
-        <p className="flex gap-2 items-center">
-          <Clock className="w-4 h-4" />
-          평균 {timeToRead}분 소요
-        </p>
+        </Link>
+        <Link href={`/blog/${slug}`} className="group">
+          <p className="text-2xl font-bold group-hover:text-primary/90 transition-colors duration-200">
+            {title}
+          </p>
+          <p className="text-lg text-muted-foreground group-hover:underline font-semibold mb-1">
+            {description}
+          </p>
+        </Link>
+        <div className="flex gap-2 mb-2">
+          {tags.map((tag) => (
+            <Link
+              href={`/blog?tag=${tag}`}
+              key={tag}
+              className="text-primary text-sm hover:underline font-semibold"
+            >
+              #{tag}
+            </Link>
+          ))}
+        </div>
+        <div className="flex gap-x-4 text-sm text-muted-foreground">
+          <p className="flex gap-1 items-center">
+            <CalendarDays className="w-4 h-4" />
+            {formatDate(date)}
+          </p>
+          <p className="flex gap-1 items-center">
+            <Clock className="w-4 h-4" />
+            평균 {timeToRead}분 소요
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
-const PostLists = ({ posts }: Props) => {
+const PostLists = ({ posts, postsLength = true }: Props) => {
   return (
     <ul className="flex flex-col w-full gap-3">
-      <p className="text-sm text-muted-foreground">
-        <span className="text-primary text-lg font-semibold">
-          {posts.length}
-        </span>
-        개의 포스트가 있습니다
-      </p>
-      {posts.map((post) => (
-        <li key={post.slug}>
-          <Link href={`blog/${post.slug}`}>
-            <PostCard
-              frontMatter={post.frontMatter}
-              timeToRead={Math.ceil(readingTime(post.content).minutes)}
-            />
-          </Link>
+      {postsLength ? (
+        <p className="text-sm text-muted-foreground">
+          <span className="text-primary text-lg font-semibold">
+            {posts.length}
+          </span>
+          개의 포스트가 있습니다
+        </p>
+      ) : null}
+      {posts.map((post, index) => (
+        <li key={post.slug} className="flex flex-col">
+          {index ? <Seperator className="w-full border-t mb-3" /> : null}
+          <PostCard
+            slug={post.slug}
+            frontMatter={post.frontMatter}
+            timeToRead={Math.ceil(readingTime(post.content).minutes)}
+          />
         </li>
       ))}
     </ul>
