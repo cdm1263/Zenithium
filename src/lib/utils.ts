06bc055ac0754ef4e26a2929fa46403806d36c5c
ChaDongmin <cdm1263@gmail.com>
2024-09-25 19:33:17 +0900

    feat: TOC에 쓰일 headings를 파싱하는 함수 parseTOCHeadings 구성

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { TOCItem } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,4 +13,20 @@ export const formatDate = (dateString: string) => {
   const currentDate = date.getUTCDate().toString().padStart(2, "0");
 
   return `${currentYear}-${currentMonth}-${currentDate}`;
+};
+
+export const parseTOCHeadings = (content: string): TOCItem[] => {
+  const headingRegex = /^(#{1,6})\s+(.+)$/gm;
+  const matches = content.matchAll(headingRegex);
+
+  return Array.from(matches).map((match) => {
+    const level = match[1].length;
+    const title = match[2].replace(/\.$/, "");
+
+    const id =
+      title.toLowerCase().replace(/\s+/g, "-") +
+      (level > 1 ? `-${level - 1}` : "");
+
+    return { id, title, level };
+  });
 };
