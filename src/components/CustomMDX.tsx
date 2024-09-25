afe80905809d89a672a1e3e742e36273d4100616
ChaDongmin <cdm1263@gmail.com>
2024-09-25 18:53:28 +0900

    feat: rehypeAddRelativeToHeadings 플러그인 적용 및 rehypeAutolinkHeadings 옵션 설정

--- src/components/CustomMDX.tsx
+++ src/components/CustomMDX.tsx
@@ -2,15 +2,32 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import rehypePrettyCode from "rehype-pretty-code";
 import remarkGfm from "remark-gfm";
 import remarkBreaks from "remark-breaks";
-import { rehypeMessageBox } from "@/lib/plugin";
+import { rehypeAddRelativeToHeadings, rehypeMessageBox } from "@/lib/plugin";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // TODO: 스타일링 필요
+
 const components = {};
 
 const CustomMDX = (props: MDXRemoteProps) => {
-  const options = {
+  const prettyCodeOptions = {
     theme: { dark: "houston", light: "github-light" },
     grid: true,
+  };
+
+  const AutoLinkOptions = {
+    content: [
+      {
+        type: "element",
+        tagName: "span",
+        properties: {
+          className:
+            "text-primary text-2xl opacity-50 hover:opacity-100 transition-opacity duration-200 absolute -left-6",
+        },
+        children: [{ type: "text", value: "#" }],
+      },
+    ],
   };
 
   return (
@@ -20,7 +37,13 @@ const CustomMDX = (props: MDXRemoteProps) => {
       options={{
         mdxOptions: {
           remarkPlugins: [remarkGfm, remarkBreaks],
-          rehypePlugins: [[rehypePrettyCode, options], [rehypeMessageBox]],
+          rehypePlugins: [
+            [rehypePrettyCode, prettyCodeOptions],
+            [rehypeMessageBox],
+            [rehypeSlug],
+            [rehypeAddRelativeToHeadings],
+            [rehypeAutolinkHeadings, AutoLinkOptions],
+          ],
         },
       }}
     />
