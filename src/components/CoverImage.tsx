c4aa6526e3015dc358091cc418e32270d7e90e1b
ChaDongmin <cdm1263@gmail.com>
2024-09-26 12:28:06 +0900

    refactor: 동적으로 이미지와 데이터를 적용하는 CoverImage 컴포넌트 리팩토링

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -1,33 +1,36 @@
-"use client";
-
-import { useState, useEffect } from "react";
+import { FrontMatter } from "@/lib/types";
 import NextImage from "next/image";
-import { usePathname } from "next/navigation";
+import Inner from "./Inner";
+import PostDateAndReadingTime from "./PostDateAndReadingTime";
 
-const CoverImage = () => {
-  const pathname = usePathname();
-  const [imageSrc, setImageSrc] = useState<string>("");
+type Props = {
+  coverData?: { imageSrc: string; title: string; description: string };
+  slug?: string;
+  frontMatter?: FrontMatter;
+  content?: string;
+};
 
-  useEffect(() => {
-    const getImageSrc = () => {
-      if (pathname === "/") {
-        return "/home-cover.jpg";
-      } else if (pathname === "/blog") {
-        return "/blog-cover.jpg";
-      } else if (pathname.startsWith("/blog/")) {
-        const slug = pathname.replace("/blog/", "");
-        return `/postAssets/${slug}/cover.jpg`;
-      }
-      return "/images/default-cover.jpg";
-    };
-
-    setImageSrc(getImageSrc());
-  }, [pathname]);
-
+const CoverImage = ({ coverData, slug, frontMatter, content }: Props) => {
   return (
-    <div className="fixed inset-x-0 top-0 h-[500px] z-0">
-      <NextImage src={imageSrc} alt="cover" fill className="object-cover" />
-      <div className="absolute inset-0 bg-black opacity-40"></div>
+    <div className={"fixed inset-x-0 top-0 h-[500px] -z-10"}>
+      <Inner className="flex flex-col absolute inset-0 z-10 justify-center text-slate-200">
+        <p className="text-5xl font-bold mb-10">
+          {coverData?.title || frontMatter?.title}
+        </p>
+        <p className="text-xl font-semibold mb-3">
+          {coverData?.description || frontMatter?.description}
+        </p>
+        {frontMatter && content && (
+          <PostDateAndReadingTime date={frontMatter.date} content={content} />
+        )}
+      </Inner>
+      <NextImage
+        src={coverData?.imageSrc || `/postAssets/${slug}/cover.jpg`}
+        alt="cover"
+        fill
+        className="object-cover z-0"
+      />
+      <div className="absolute inset-0 bg-black opacity-40" />
     </div>
   );
 };
