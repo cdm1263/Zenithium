3e96e0523de9bc58b6ca716fb924590ca3da4b4e
ChaDongmin <cdm1263@gmail.com>
2024-09-26 11:10:00 +0900

    refactor: CoverImage가 최상단에 fix되는 형태로 변경 및 내부에서 이미지 관리

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -1,19 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import NextImage from "next/image";
+import { usePathname } from "next/navigation";
 
-type Props = {
-  imageSrc?: string;
-  slug?: string;
-};
+const CoverImage = () => {
+  const pathname = usePathname();
+  const [imageSrc, setImageSrc] = useState<string>("");
 
-const CoverImage = ({ imageSrc, slug }: Props) => {
+  useEffect(() => {
+    const getImageSrc = () => {
+      if (pathname === "/") {
+        return "/home-cover.jpg";
+      } else if (pathname === "/blog") {
+        return "/blog-cover.jpg";
+      } else if (pathname.startsWith("/blog/")) {
+        const slug = pathname.replace("/blog/", "");
+        return `/postAssets/${slug}/cover.jpg`;
+      }
+      return "/images/default-cover.jpg";
+    };
+
+    setImageSrc(getImageSrc());
+  }, [pathname]);
+
   return (
-    <div className="absolute inset-x-0 top-0 h-80 ">
-      <NextImage
-        src={imageSrc || `/postAssets/${slug}/cover.jpg`}
-        alt="cover"
-        fill
-        className="object-cover"
-      />
+    <div className="fixed inset-x-0 top-0 h-[500px] z-0">
+      <NextImage src={imageSrc} alt="cover" fill className="object-cover" />
       <div className="absolute inset-0 bg-black opacity-40"></div>
     </div>
   );
