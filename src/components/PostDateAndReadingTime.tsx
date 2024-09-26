837ada8b880728077d3e7075cf1abed4f5b4a878
ChaDongmin <cdm1263@gmail.com>
2024-09-27 04:30:26 +0900

    refactor: PostDateAndReadingTime의 각 텍스트 스타일을 추가로 지정 할 수 있도록 option 추가

--- src/components/PostDateAndReadingTime.tsx
+++ src/components/PostDateAndReadingTime.tsx
@@ -1,20 +1,27 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { CalendarDays, Clock } from "lucide-react";
 import readingTime from "reading-time";
 
 type Props = {
   date: string;
   content: string;
+  dateClassName?: string;
+  readingClassName?: string;
 };
 
-const PostDateAndReadingTime = ({ date, content }: Props) => {
+const PostDateAndReadingTime = ({
+  date,
+  content,
+  dateClassName,
+  readingClassName,
+}: Props) => {
   return (
     <div className="flex gap-x-4 text-sm text-muted-foreground">
-      <p className="flex gap-1 items-center">
+      <p className={cn("flex gap-1 items-center", dateClassName)}>
         <CalendarDays className="w-4 h-4" />
         {formatDate(date)}
       </p>
-      <p className="flex gap-1 items-center">
+      <p className={cn("flex gap-1 items-center", readingClassName)}>
         <Clock className="w-4 h-4" />
         평균 {Math.ceil(readingTime(content).minutes)}분 소요
       </p>
