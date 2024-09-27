1d6b3b210c38abe3595da0fd1f981fd70a784427
ChaDongmin <cdm1263@gmail.com>
2024-09-28 00:20:50 +0900

    feat: CoverImage에 useSpring 효과 적용

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -4,7 +4,7 @@ import { FrontMatter } from "@/lib/types";
 import NextImage from "next/image";
 import Inner from "./Inner";
 import PostDateAndReadingTime from "./PostDateAndReadingTime";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 type Props = {
   coverData?: { imageSrc: string; title: string; description: string };
@@ -15,9 +15,15 @@ type Props = {
 
 const CoverImage = ({ coverData, slug, frontMatter, content }: Props) => {
   const { scrollY } = useScroll();
-  const scale = useTransform(scrollY, [0, 500], [1, 1.1]);
-  const y = useTransform(scrollY, [0, 500], [0, -100]);
-  const opacity = useTransform(scrollY, [0, 500], [1, 0]);
+  const srpingOptions = { stiffness: 200, damping: 20 };
+
+  const transfromedScale = useTransform(scrollY, [0, 500], [1, 1.1]);
+  const transfromedY = useTransform(scrollY, [0, 500], [0, -100]);
+  const transfromedOpacity = useTransform(scrollY, [0, 500], [1, 0]);
+
+  const scale = useSpring(transfromedScale, srpingOptions);
+  const y = useSpring(transfromedY, srpingOptions);
+  const opacity = useSpring(transfromedOpacity, srpingOptions);
 
   return (
     <div className="fixed inset-x-0 top-0 h-[500px] -z-10 overflow-hidden">
