fd4ffec152f582abe5907323e9d9e582f6898534
ChaDongmin <cdm1263@gmail.com>
2024-09-27 23:54:47 +0900

    refactor: framer motion을 사용한 FloatingScroller 재구성

--- src/components/FloatingScroller.tsx
+++ src/components/FloatingScroller.tsx
@@ -1,34 +1,45 @@
 "use client";
 
-import { ArrowDown, ArrowUp } from "lucide-react";
-import { Button } from "./ui/button";
-import { cn } from "@/lib/utils";
+import { ArrowUp } from "lucide-react";
+import { motion, useScroll, useSpring } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const FloatingScroller = () => {
-  const handleScroll = (top: number) => {
-    window.scroll({
-      top,
-      behavior: "smooth",
-    });
+  const { scrollYProgress } = useScroll();
+  const progress = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
+  const [progressValue, setProgressValue] = useState(0);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const buttonStyle =
-    "p-2 w-10 border border-muted-foreground hover:scale-110 transition-all";
+  useEffect(() => {
+    return progress.on("change", (value) => {
+      setProgressValue(value);
+    });
+  }, [progress]);
+
   return (
-    <div className="fixed flex flex-col right-8 bottom-8 space-y-1">
-      <Button
-        onClick={() => handleScroll(0)}
-        className={cn("rounded-b-none", buttonStyle)}
-      >
-        <ArrowUp className="text-gray-200" />
-      </Button>
-      <Button
-        onClick={() => handleScroll(document.body.scrollHeight)}
-        className={cn("rounded-t-none", buttonStyle)}
-      >
-        <ArrowDown className="text-gray-200" />
-      </Button>
-    </div>
+    <motion.div
+      onClick={scrollToTop}
+      className="w-12 h-12 fixed bottom-8 right-8 rounded-full flex items-center justify-center hover:bg-primary cursor-pointer group"
+      style={{
+        background: `conic-gradient(
+            hsl(var(--primary) / 0.6) ${progressValue * 360}deg, 
+            hsla(var(--foreground) / 0.1) ${progressValue * 360}deg
+          )`,
+      }}
+      whileHover={{
+        background: `conic-gradient(
+            hsl(var(--primary) / 1) ${progressValue * 360}deg, 
+            hsla(var(--foreground) / 0.1) ${progressValue * 360}deg
+        )`,
+      }}
+    >
+      <div className="w-10 h-10 bg-background rounded-full flex items-center justify-center">
+        <ArrowUp className="text-primary/60 group-hover:text-primary transition-colors duration-150 w-8 h-8" />
+      </div>
+    </motion.div>
   );
 };
 
