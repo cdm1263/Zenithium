1523e4706eb0c0bb7060062ae421be31d935db63
ChaDongmin <cdm1263@gmail.com>
2024-09-29 00:57:32 +0900

    feat: 이전, 다음 포스트로 이동하는 NeighborPosts 구성

---
+++ src/components/NeighborPosts.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+type Props = {
+  beforePost: { slug: string; title: string } | null;
+  afterPost: { slug: string; title: string } | null;
+};
+
+const NeighborPosts = ({ beforePost, afterPost }: Props) => {
+  return (
+    <div className="max-w-screen-sm self-center w-full flex my-10 min-h-20 items-center justify-between">
+      {beforePost ? (
+        <Link
+          className="flex flex-col gap-2 max-w-[40%] min-w-[100px] group"
+          href={`/blog/${beforePost.slug}`}
+        >
+          <span className="text-sm text-muted-foreground/70 group-hover:text-muted-foreground/100 transition-colors">
+            이전 포스트
+          </span>
+          <span className="text-primary/70 group-hover:text-primary/100 transition-colors">
+            {beforePost.title}
+          </span>
+        </Link>
+      ) : (
+        <div className="flex-1" />
+      )}
+
+      {afterPost && (
+        <Link
+          className="flex flex-col gap-2 max-w-[40%] min-w-[100px] items-end group"
+          href={`/blog/${afterPost.slug}`}
+        >
+          <span className="text-sm text-muted-foreground/70 group-hover:text-muted-foreground/100 transition-colors">
+            다음 포스트
+          </span>
+          <span className="text-primary/70 group-hover:text-primary/100 transition-colors">
+            {afterPost.title}
+          </span>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default NeighborPosts;
