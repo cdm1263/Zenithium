f8e1fad3658deb78cdba3a144b9c632c14c31a84
ChaDongMin <cdm1263@gmail.com>
2024-09-29 19:37:38 +0900

    feat: RSS를 생성하는 route handler 구성

---
+++ src/app/api/rss/route.ts
@@ -0,0 +1,34 @@
+import { getAllPosts } from "@/app/blog/utils";
+import { baseUrl } from "@/app/sitemap";
+import RSS from "rss";
+
+export const GET = () => {
+  const allPosts = getAllPosts();
+
+  const feed = new RSS({
+    title: "Zenithium",
+    description: "To Zenith",
+    generator: "RSS for Zenithium",
+    site_url: baseUrl,
+    feed_url: `${baseUrl}/feed.xml`,
+    copyright: `Copyright ${new Date().getUTCFullYear().toString()}`,
+    language: "ko-KR",
+    pubDate: new Date().toUTCString(),
+    ttl: 60,
+  });
+
+  allPosts.map(({ frontMatter: { title, description, date }, slug }) => {
+    feed.item({
+      title,
+      description,
+      date,
+      url: `${baseUrl}/blog/${slug}`,
+    });
+  });
+
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+};
