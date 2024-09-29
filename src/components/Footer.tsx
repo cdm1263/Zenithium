e3c62c94977dedf7b46f57c5c504bbc5bf97e952
ChaDongMin <cdm1263@gmail.com>
2024-09-29 19:50:20 +0900

    fix: Footer RSS 링크 컴포넌트 위치 조정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,14 +7,16 @@ import Link from "next/link";
 
 const Footer = () => {
   const iconStyle =
-    "h-7 w-7 text-foreground hover:text-muted-foreground transition-all cursor-pointer";
+    "h-7 w-7 text-foreground hover:text-muted-foreground transition-all";
 
   return (
     <footer className="bg-muted h-60">
       <Inner className="flex flex-col justify-center items-center">
-        <Link href="/api/rss" className="flex gap-x-5 mb-10">
+        <div className="flex gap-x-5 mb-10">
           {/* // TODO: 추후 Link로 변경해야함 */}
-          <Rss className={iconStyle} />
+          <Link href="/api/rss">
+            <Rss className={iconStyle} />
+          </Link>
           <SVGtoComponent
             SVG={Github}
             className={cn(
@@ -22,7 +24,7 @@ const Footer = () => {
               "fill-foreground hover:fill-muted-foreground"
             )}
           />
-        </Link>
+        </div>
         <p className="text-lg text-muted-foreground">
           &copy; {new Date().getFullYear()}, All rights reserved.
         </p>
