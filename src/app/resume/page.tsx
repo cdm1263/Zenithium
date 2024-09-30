82d93f29588773b9296fb0ab082aeb9db0c4035c
ChaDongmin <cdm1263@gmail.com>
2024-10-01 07:17:13 +0900

    feat: resume 페이지에 CoverImage 적용

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,21 @@
+import CoverImage from "@/components/CoverImage";
+
 export const metadata = {
   title: "Resume",
   description: "To Zenith",
 };
 
 const Resume = () => {
-  return <div>Resume</div>;
+  const coverData = {
+    imageSrc: "/resume-cover.jpg",
+    title: "Resume",
+    description: "안녕하세요, 저는 이런 개발자 입니다.",
+  };
+  return (
+    <section>
+      <CoverImage coverData={coverData} />
+    </section>
+  );
 };
 
 export default Resume;
