c355eaa2aceaeae9ff31cc2653ae9812cdbf92ec
ChaDongmin <cdm1263@gmail.com>
2024-10-02 06:44:09 +0900

    refactor: SocialIcons의 중복 코드 리팩토링 및 노션 아이콘 적용

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -3,12 +3,43 @@
 import SVGtoComponent from "@/components/SVGtoComponent";
 import Github from "/public/github.svg";
 import LinkedIn from "/public/linkedin.svg";
+import Notion from "/public/notion.svg";
 import { Mail } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
+import { FunctionComponent, SVGProps } from "react";
 
+const SVGLink = ({
+  href,
+  SVG,
+  text,
+}: {
+  href: string;
+  SVG: FunctionComponent<SVGProps<SVGSVGElement>>;
+  text?: string;
+}) => (
+  <Link
+    href={href}
+    target="_blank"
+    className={cn(
+      buttonVariants({
+        variant: "outline",
+        size: "sm",
+      }),
+      "group px-2",
+      text &&
+        "flex items-center gap-x-2 font-semibold text-sm hover:text-primary"
+    )}
+  >
+    <SVGtoComponent
+      SVG={SVG}
+      className="h-5 w-5 text-foreground fill-foreground group-hover:fill-primary transition-colors"
+    />
+    {text}
+  </Link>
+);
 const SocialIcons = () => {
   const email = "cdm1263@gmail.com";
   const { toast } = useToast();
@@ -29,39 +60,10 @@ const SocialIcons = () => {
   };
 
   return (
-    <div className="flex items-center gap-x-2">
-      <Link
-        href="https://github.com/cdm1263"
-        target="_blank"
-        className={cn(
-          buttonVariants({
-            variant: "outline",
-            size: "sm",
-          }),
-          "group px-2"
-        )}
-      >
-        <SVGtoComponent
-          SVG={Github}
-          className="h-5 w-5 text-foreground fill-foreground group-hover:fill-primary transition-colors"
-        />
-      </Link>
-      <Link
-        href="https://www.linkedin.com/in/dongmincha/"
-        target="_blank"
-        className={cn(
-          buttonVariants({
-            variant: "outline",
-            size: "sm",
-          }),
-          "group px-2"
-        )}
-      >
-        <SVGtoComponent
-          SVG={LinkedIn}
-          className="h-5 w-5 text-foreground fill-foreground group-hover:fill-primary transition-colors"
-        />
-      </Link>
+    <div className="flex items-center gap-x-2 flex-wrap">
+      <SVGLink href="https://github.com/cdm1263" SVG={Github} />
+      <SVGLink href="https://www.linkedin.com/in/dongmincha/" SVG={LinkedIn} />
+
       <button
         onClick={handleCopy}
         className={cn(
@@ -74,6 +76,12 @@ const SocialIcons = () => {
       >
         <Mail className="h-5 w-5 group-hover:text-primary transition-colors" />
       </button>
+
+      <SVGLink
+        href="https://dongmindev.notion.site/6e6ae85ddb5841e5af81b5139ce1ffea?pvs=4"
+        SVG={Notion}
+        text="Notion Portfolio"
+      />
     </div>
   );
 };
