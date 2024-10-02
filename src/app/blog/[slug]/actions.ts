0b5cf823cad64931f53cde6c964022a3b29a04db
ChaDongmin <cdm1263@gmail.com>
2024-10-02 17:37:54 +0900

    feat: slug 조회수를 호출하는 fetch 함수 구성

---
+++ src/app/blog/[slug]/actions.ts
@@ -0,0 +1,14 @@
+import { baseUrl } from "@/app/sitemap";
+import { unstable_noStore as noStore } from "next/cache";
+
+export const getPageViews = async (slug: string) => {
+  noStore();
+  const res = await fetch(`${baseUrl}/api/pageviews?path=/blog/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("페이지 조회수 호출 실패");
+  }
+
+  const { pageViews } = await res.json();
+  return pageViews;
+};
