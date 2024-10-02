555b34bad72f29d6bb20a64a2317ba1843f290fd
ChaDongmin <cdm1263@gmail.com>
2024-10-02 21:08:00 +0900

    design: og Image 폰트 적용 및 텍스트 크기 수정

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -12,17 +12,29 @@ export const GET = async (req: Request) => {
     bgPath ? `postAssets/${bgPath}/cover.jpg` : "home-cover.jpg"
   }`;
 
+  const font = await fetch(
+    new URL(`${baseUrl}/font/NanumSquareRoundB.ttf`)
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(
     (
       <div tw="flex w-full h-full items-center justify-center relative">
         <img src={bgSrc} alt={`${title} 이미지`} tw="absolute inset-0" />
         <div tw="absolute inset-0 bg-black opacity-50" />
         <div tw="flex flex-col w-full py-12 px-24">
-          <h2 tw="text-white text-5xl font-bold mb-3 break-keep">{title}</h2>
-          <p tw="text-white text-xl font-semibold break-keep">{description}</p>
+          <h2 tw="text-white text-[100px] font-bold mb-3 break-keep">
+            {title}
+          </h2>
+          <p tw="text-white text-[36px] font-semibold break-keep">
+            {description}
+          </p>
         </div>
       </div>
     ),
-    { width: 1200, height: 630 }
+    {
+      width: 1200,
+      height: 630,
+      fonts: [{ name: "NanumSquareRoundB", data: font }],
+    }
   );
 };
