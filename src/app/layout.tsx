1e0018aa0dcf6d1b2c90b8c2c2fdb196bf326359
ChaDongmin <cdm1263@gmail.com>
2024-10-02 18:11:59 +0900

    refactor: GoogleAnalytics 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import FloatingScroller from "@/components/FloatingScroller";
 import localFont from "next/font/local";
 import { baseUrl } from "./sitemap";
 import { Toaster } from "@/components/ui/toaster";
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const nanumSquareRound = localFont({
   src: [
@@ -95,7 +95,6 @@ export default function RootLayout({
           <Footer />
           <FloatingScroller />
           <Toaster />
-          <GoogleTagManager gtmId={gaId} />
           <GoogleAnalytics gaId={gaId} />
         </ThemeProvider>
       </body>
