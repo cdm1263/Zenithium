829d9a17d0fda1911a62d11e206d7841427b71bc
ChaDongmin <cdm1263@gmail.com>
2024-10-03 09:39:37 +0900

    fix: ppr 미사용

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    ppr: true,
-  },
   // Note: svg를 컴포넌트로 사용
   webpack(config) {
     config.module.rules.push({
