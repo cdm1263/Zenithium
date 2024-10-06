c203ff81abb02676857231ee35c78081d4ed159f
ChaDongmin <cdm1263@gmail.com>
2024-10-07 08:11:27 +0900

    refactor: getPageViews 함수의 재검증을 1시간마다 하도록 캐싱

--- src/app/blog/[slug]/actions.ts
+++ src/app/blog/[slug]/actions.ts
@@ -1,17 +1,18 @@
 "use server";
 
 import { baseUrl } from "@/app/sitemap";
-import { unstable_noStore as noStore, revalidatePath } from "next/cache";
 
 export const getPageViews = async (slug: string) => {
-  noStore();
-  const res = await fetch(`${baseUrl}/api/pageviews?path=/blog/${slug}`);
+  const res = await fetch(`${baseUrl}/api/pageviews?path=/blog/${slug}`, {
+    next: {
+      revalidate: 60 * 60 * 1,
+    },
+  });
 
   if (!res.ok) {
     throw new Error("페이지 조회수 호출 실패");
   }
 
   const { pageViews } = await res.json();
-  revalidatePath(`/blog/${slug}`);
   return pageViews;
 };
