3bbe2fa0e0bfea20f37dd44748b28e597e86823f
ChaDongmin <cdm1263@gmail.com>
2024-10-07 08:12:41 +0900

    docs: getPageViews 주석 설정

--- src/app/blog/[slug]/actions.ts
+++ src/app/blog/[slug]/actions.ts
@@ -4,6 +4,7 @@ import { baseUrl } from "@/app/sitemap";
 
 export const getPageViews = async (slug: string) => {
   const res = await fetch(`${baseUrl}/api/pageviews?path=/blog/${slug}`, {
+    // Note: 캐시 재검증을 1시간 단위로 하도록 해 조회수의 최신화가 1시간 간격으로 동작하도록 설정
     next: {
       revalidate: 60 * 60 * 1,
     },
