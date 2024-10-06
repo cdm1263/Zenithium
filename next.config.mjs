dc2036270d7497c43c63c000395127b884a0c147
ChaDongmin <cdm1263@gmail.com>
2024-10-07 06:41:06 +0900

    feat: ppr 활성화

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
   // Note: svg를 컴포넌트로 사용
   webpack(config) {
     config.module.rules.push({
